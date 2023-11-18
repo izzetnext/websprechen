f00abd965b2ce35d073d0c613b701554a9e2ac83
izzetnext <126400918+izzetnext@users.noreply.github.com>
2023-11-18 15:25:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     var recognition = new webkitSpeechRecognition();
     recognition.lang = 'tr-TR'; // Türkçe dil ayarı
-    recognition.continuous = false;
-    recognition.interimResults = false;
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    var timeoutId;
+    var transcript = '';
 
     recognition.onresult = function(event) {
-        var transcript = event.results[0][0].transcript;
+        // Mevcut zamanlayıcıyı sıfırla
+        clearTimeout(timeoutId);
+
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            transcript += event.results[i][0].transcript;
+        }
         document.getElementById('transcript').textContent = transcript;
+
+        // 2 saniye bekleyip yeni satıra geç
+        timeoutId = setTimeout(function() {
+            transcript += '\n'; // Yeni satıra geç
+            document.getElementById('transcript').textContent = transcript;
+        }, 2000); // 2 saniye beklemeyi ayarla
+    };
+
+    recognition.onend = function() {
+        recognition.start(); // Dinleme sona erdiğinde tekrar başlat
     };
 
     document.getElementById('start-btn').addEventListener('click', function() {
