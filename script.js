687c8afeecdcd3ae20d1eb59d0fc8feea2472093
izzetnext <126400918+izzetnext@users.noreply.github.com>
2023-11-18 15:42:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var timeoutId;
     var transcript = '';
-    var isListening = false; // Dinleme durumu
-    var startButton = document.getElementById('start-btn');
+    var isListening = false; 
+    var toggleButton = document.getElementById('toggle-listening');
+    var transcriptArea = document.getElementById('transcript');
+    var saveButton = document.getElementById('save-text');
+    var emailButton = document.getElementById('send-email');
 
     recognition.onresult = function(event) {
         clearTimeout(timeoutId);
@@ -15,26 +18,25 @@ document.addEventListener("DOMContentLoaded", function() {
         for (var i = event.resultIndex; i < event.results.length; ++i) {
             transcript += event.results[i][0].transcript;
         }
-        document.getElementById('transcript').textContent = transcript;
+        transcriptArea.value = transcript;
 
         timeoutId = setTimeout(function() {
             transcript += '\n';
-            document.getElementById('transcript').textContent = transcript;
+            transcriptArea.value = transcript;
         }, 2000);
     };
 
     recognition.onstart = function() {
         isListening = true;
-        updateButton();
+        updateToggleButton();
     };
 
     recognition.onend = function() {
         isListening = false;
-        updateButton();
-        recognition.start(); // Sürekli dinlemeyi sağla
+        updateToggleButton();
     };
 
-    startButton.addEventListener('click', function() {
+    toggleButton.addEventListener('click', function() {
         if (isListening) {
             recognition.stop();
         } else {
@@ -42,13 +44,24 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    function updateButton() {
+    function updateToggleButton() {
         if (isListening) {
-            startButton.textContent = 'Dinleniyor...';
-            startButton.style.backgroundColor = 'red';
+            toggleButton.textContent = 'Dinleniyor...';
+            toggleButton.style.backgroundColor = 'red';
         } else {
-            startButton.textContent = 'Dinlemeyi Başlat';
-            startButton.style.backgroundColor = 'green';
+            toggleButton.textContent = 'Dinlemeyi Başlat/Durdur';
+            toggleButton.style.backgroundColor = 'green';
         }
     }
+
+    saveButton.addEventListener('click', function() {
+        var textToSave = transcriptArea.value;
+        var blob = new Blob([textToSave], { type: "text/plain;charset=utf-8" });
+        saveAs(blob, "transcript.txt"); // 'FileSaver.js' kütüphanesi gereklidir
+    });
+
+    emailButton.addEventListener('click', function() {
+        var emailBody = encodeURIComponent(transcriptArea.value);
+        window.open('mailto:test@example.com?subject=Konuşma Metni&body=' + emailBody);
+    });
 });
